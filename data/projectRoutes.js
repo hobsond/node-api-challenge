b4d219dc2a6d663175bba1dd928220900bafd262
D'onta Hobson <hobsond26@icloud.com>
2020-06-05 12:47:26 -0400

    created get actions route

--- data/projectRoutes.js
+++ data/projectRoutes.js
@@ -38,4 +38,57 @@ router.get('/:id',(req,res)=>{
     .catch(err=>res.status(400).json({error:'could not find user at that id'}))
 })
 
+router.put('/:id',(req,res)=>{
+    const id = req.params.id
+    Db.get(id)
+    .then(item=>{
+        if(item){
+            Db.update(id, req.body)
+                .then(item => {
+                    Db.get(id)
+                        .then(projects => res.status(200).json(projects))
+                })
+
+
+                .catch(err => res.status(500).json({ error: 'could not update user' }))
+
+        }
+    })
+        .catch(() => res.status(400).json({ error: 'can not find user with that id' }))
+   
+})
+
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id
+    Db.remove(id)
+    .then(item=>{
+     Db.get()
+     .then(projects=>res.status(200).json(projects))
+    })
+    .catch(err=>res.status(400).json({error:'can not delete user'}))
+})
+
+router.get('/:id/actions',(req,res)=>{
+    const id = req.params.id
+
+    Db.get(id)
+    .then(user=>{
+        if(user){
+            Db.getProjectActions(id)
+                .then(item => {
+                    res.status(200).json(item)
+
+                })
+                .catch(() => {
+                    res.status(400).json({ error: 'Can not fint that project ' })
+                })
+
+        } else {
+            res.status(500).json({ error: 'can not find project with id' })
+        }
+    })
+
+   
+})
+
 module.exports = router
